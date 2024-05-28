be955f6ea2a5452a5ff16338fdce74c59089522c
developer-mixa <mixail_081177@mail.ru>
2024-05-28 17:01:33 +0300

    feat: add tests for views

--- cinephile_server/views.py
+++ cinephile_server/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.core.handlers.wsgi import WSGIRequest
-from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, CinemaSerializer, FilmSerializer, TicketSerializer, FilmCinemaSerializer
 from .models import Cinema, Film, Ticket, FilmCinema
@@ -34,12 +33,12 @@ class TicketViewSet(LoginRequired, ModelViewSet):
 
 # helpers
 
-def generate_html_page(request, template_name, context=None, extension='html'):
+def __generate_html_page(request, template_name, context=None, extension='html'):
     if context is None:
         context = {}
     return render(request, f'{template_name}.{extension}', context)
 
-def get_tickets_by_film_cinema(model: Film | Cinema) -> list[Ticket]:
+def __get_tickets_by_film_cinema(model: Film | Cinema) -> list[Ticket]:
     film_cinemas = model.filmcinema_set.all()
     tickets = Ticket.objects.prefetch_related(
         Prefetch('film_cinema', queryset=film_cinemas, to_attr='film_cinemas')
@@ -49,33 +48,33 @@ def get_tickets_by_film_cinema(model: Film | Cinema) -> list[Ticket]:
 # pages
 
 def main_page(request):
-    return generate_html_page(request, template.INDEX)
+    return __generate_html_page(request, template.INDEX)
 
 def films_page(request: WSGIRequest):
-    return generate_html_page(request, template.FILMS, {template.FILMS: Film.objects.all()})
+    return __generate_html_page(request, template.FILMS, {template.FILMS: Film.objects.all()})
 
 def cinemas_page(request: WSGIRequest):
-    return generate_html_page(request, template.CINEMAS, {template.CINEMAS: Cinema.objects.all()})
+    return __generate_html_page(request, template.CINEMAS, {template.CINEMAS: Cinema.objects.all()})
 
 def film_detail_page(request: WSGIRequest, pk):
     film = Film.objects.get(id=pk)
-    tickets = get_tickets_by_film_cinema(film)
-    return generate_html_page(request, template.FILM_DETAILS, {'film': film, 'tickets': tickets})
+    tickets = __get_tickets_by_film_cinema(film)
+    return __generate_html_page(request, template.FILM_DETAILS, {'film': film, 'tickets': tickets})
 
 def cinema_detail_page(request: WSGIRequest, pk):
     cinema = Cinema.objects.get(id=pk)
-    tickets = get_tickets_by_film_cinema(cinema)
-    return generate_html_page(request, template.CINEMA_DETAILS, {'cinema': cinema, 'tickets': tickets})
+    tickets = __get_tickets_by_film_cinema(cinema)
+    return __generate_html_page(request, template.CINEMA_DETAILS, {'cinema': cinema, 'tickets': tickets})
 
 def booked_tickets_page(request: WSGIRequest):
     if request.user.is_authenticated:
         user = request.user
         tickets = Ticket.objects.filter(user=user)
-        return generate_html_page(request, template.BOOKED_TICKETS, {'tickets': tickets})
+        return __generate_html_page(request, template.BOOKED_TICKETS, {'tickets': tickets})
     return redirect(template.PROFILE)
 
 def profile_page(request: WSGIRequest):
-    return generate_html_page(request, template.PROFILE, {'user': request.user})
+    return __generate_html_page(request, template.PROFILE, {'user': request.user})
 
 def register_page(request: WSGIRequest):
     errors = ''
@@ -89,7 +88,7 @@ def register_page(request: WSGIRequest):
             errors = form.errors
     else:
         form = RegistrationForm()
-    return generate_html_page(
+    return __generate_html_page(
         request,
         template.REGISTER,
         {'form': form, 'errors': errors},
